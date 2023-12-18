1cad7fcae4f486dc03711a0772034bf0caf88308
zhanghengxin <ezreal.zhang@icewhale.org>
2023-12-18 17:53:38 +0800

    i18n:4

--- src/views/EstablishRAID/CreatingRAIDPart.vue
+++ src/views/EstablishRAID/CreatingRAIDPart.vue
@@ -2,13 +2,14 @@
 import { LottieAnimation } from "lottie-web-vue";
 import creatingJSON from "@assets/img/EstablishRAID/creating.json";
 import EasterEgg from "@assets/img/EstablishRAID/easterEgg.json";
+import { context } from "./controlData.ts";
 </script>
 <template name="CreatingRAIDPart">
     <div class="flex flex-col items-center space-y-2 pt-[5%]">
         <LottieAnimation :animation-data="creatingJSON" loop class="w-[6.25rem] h-[6.25rem]" />
         <LottieAnimation :animation-data="EasterEgg" loop v-if="false" />
         <span class="text-zinc-800 text-base font-semibold font-['Roboto']">
-            {{ $t("Creating...") }}
+            {{ context === 'FirstAid' ? $t('Recovery in progress') : $t("Creating...") }}
         </span>
     </div>
     <div class="mt-3">
