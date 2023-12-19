f5bfa568c40a8823e5e6c6fd607cb3c95ab957a4
zhanghengxin <812718649@qq.com>
2023-12-19 16:40:03 +0800

    :fire: remove invalid code.
    * :fire: remove invalid code.

--- src/views/StorageManager/ZimaCubeCard.vue
+++ src/views/StorageManager/ZimaCubeCard.vue
@@ -2,20 +2,6 @@
 // import ZimaCubeHubSketch from './ZimaCubeHubSketch.vue'
 import ZimaCubeSketch from "./ZimaCubeSketch.vue";
 import DiskCard from "./DiskCard.vue";
-// import { ref, onMounted } from 'vue'
-// let ZimaCubeSketchRef = ref(null)
-// // 根据 svg id 获取 svg 元素，并更改其opacity属性
-// const setOpacity = (id: string, opacity: number) => {
-//     debugger
-//     const svg = ZimaCubeSketchRef.value.getElementById(id)
-//     if (svg) {
-//         svg.setAttribute('opacity', opacity.toString())
-//     }
-// }
-// // setOpacity('test', 0.5)
-// onMounted(() => {
-//     setOpacity('test', 0.5)
-// })
 import { HDDStatus, SSDStatus } from "./controlData.ts";
 
 const HDD: string[] = ["1", "2", "3", "4", "5", "6"];
