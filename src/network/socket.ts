96fd4c2603c00f97bd229f0861457106e496a5ef
zhanghengxin <ezreal.zhang@icewhale.org>
2023-12-20 19:05:19 +0800

    Update tsconfig.node.json and package.json, delete unused socket.ts file, and add socket event listeners in StorageManager.vue

---
+++ src/network/socket.ts
@@ -0,0 +1,17 @@
+// TODO: 后端升级到socket.io 4.0.0后，需要修改socket.io-client版本为4.0.0
+import io from "socket.io-client";
+
+const protocol = document.location.protocol;
+const wsProtocol = protocol === "https:" ? "wss:" : "ws:";
+const openApiDevIP = import.meta.env.VITE_OPEN_API_DEV_IP;
+const openApiDevPort = import.meta.env.VITE_OPEN_API_DEV_PORT;
+const baseURL = `${openApiDevIP}:${openApiDevPort}`;
+const wsURL = `${wsProtocol}//${baseURL}`;
+
+export const socket = io(wsURL, {
+    // transports: ["websocket", "polling"],
+    transports: ["websocket"],
+    path: "/v2/message_bus/socket.io",
+    reconnection: false,
+    autoConnect: true,
+})
