3a4802d11b542900446105fcba4aa846bf4abb57
zhanghengxin <ezreal.zhang@icewhale.org>
2023-12-25 16:09:35 +0800

    Refactor menu bar and add language setting event listener

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <script setup lang="ts">
-import Splitter from 'primevue/splitter';
-import SplitterPanel from 'primevue/splitterpanel';
-import menuPart from '@pages/App/menuPart.vue';
-import contentPart from '@pages/App/contentPart.vue';
-import DynamicDialog from 'primevue/dynamicdialog';
+import Splitter from "primevue/splitter";
+import SplitterPanel from "primevue/splitterpanel";
+import menuPart from "@pages/App/menuPart.vue";
+import contentPart from "@pages/App/contentPart.vue";
+import DynamicDialog from "primevue/dynamicdialog";
 </script>
 
 <template>
-	<Splitter class="os_panel">
-		<!-- menu bar start -->
-		<SplitterPanel :size="20" class="panel_left">
-			<menuPart></menuPart>
-		</SplitterPanel>
-		<!-- menu bar end -->
+    <Splitter class="os_panel">
+        <!-- menu bar start -->
+        <SplitterPanel :size="20" class="panel_left">
+            <menuPart></menuPart>
+        </SplitterPanel>
+        <!-- menu bar end -->
 
-		<SplitterPanel :size="80" class="panel_right">
+        <SplitterPanel :size="80" class="panel_right">
             <contentPart></contentPart>
-		</SplitterPanel>
-	</Splitter>
-    <DynamicDialog class="opacity-40 bg-black"/>
+        </SplitterPanel>
+    </Splitter>
+    <DynamicDialog class="opacity-40 bg-black" />
 </template>
 
 <style scoped></style>
