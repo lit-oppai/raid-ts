5d6374f6efb60723c95e295c4fbd21611fabd7bb
zhanghengxin <ezreal.zhang@icewhale.org>
2024-01-05 19:15:19 +0800

    Fix router navigation issue in DiscoverNewHardDrive.vue

--- src/views/DiscoverNewHardDrive/controlData.ts
+++ src/views/DiscoverNewHardDrive/controlData.ts
@@ -2,8 +2,8 @@ import { ref } from "vue";
 import { disk } from "@network/index.ts";
 import { IndexForDiskHubMap } from "@views/StorageManager/controlData.ts";
 const allNewDiskStatus = ref(new Map());
-const loadAllNewDiskStatus = (): void => {
-    disk.getDisks("show").then((res) => {
+const loadAllNewDiskStatus = async (): Promise<void> => {
+    return disk.getDisks("show").then((res) => {
         if (res.status === 200 && res.data.data) {
             allNewDiskStatus.value = new Map(
                 res.data.data
