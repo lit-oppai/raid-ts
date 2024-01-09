3deb607cc673c2fdafd062948faddde2437e1845
zhanghengxin <ezreal.zhang@icewhale.org>
2024-01-09 19:27:11 +0800

    Refactor code to remove unnecessary conditional statement and import vue-i18n

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { createApp, App } from 'vue'
+import { createApp } from 'vue'
 import '@style/style.css'
 import primeStyle from './style/prime.ts'
 import app from './App.vue'
@@ -7,32 +7,13 @@ import router from '@pages/router.ts'
 import PrimeVue from 'primevue/config'
 import DialogService from 'primevue/dialogservice'
 
-if (window.__POWERED_BY_WUJIE__) {
-    let instance: App | undefined
-    window.__WUJIE_MOUNT = () => {
-        instance = createApp(app)
-            .use(PrimeVue, {
-                unstyled: true,
-                pt: primeStyle // theme
-                // pt: {}
-            })
-            .use(router)
-            .use(i18n)
-            .use(DialogService)
-        instance.mount('#icewhale-settings-app')
-    }
-    window.__WUJIE_UNMOUNT = () => {
-        instance && instance.unmount()
-    }
-} else {
-    createApp(app)
-        .use(PrimeVue, {
-            unstyled: true,
-            pt: primeStyle // theme
-            // pt: {}
-        })
-        .use(router)
-        .use(i18n)
-        .use(DialogService)
-        .mount('#icewhale-settings-app')
-}
+createApp(app)
+    .use(PrimeVue, {
+        unstyled: true,
+        pt: primeStyle // theme
+        // pt: {}
+    })
+    .use(router)
+    .use(i18n)
+    .use(DialogService)
+    .mount('#icewhale-settings-app')
