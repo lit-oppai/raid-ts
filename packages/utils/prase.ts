cb757bd6dc40c4589260fd45cb5adc88c6b018b6
zhanghengxin <ezreal.zhang@icewhale.org>
2024-01-18 09:10:18 +0000

    We are initiating the monprepo today.

---
+++ packages/utils/prase.ts
@@ -0,0 +1,46 @@
+// 单位转换:bit 转 GB、TB、PB、EB、ZB、YB
+export const convertSizeToReadable = (size: number | string | undefined = 0) => {
+    const unit = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    let index = 0
+    if (typeof size === 'string') {
+        size = Number(size)
+        // 检测字符串是否溢出
+        if (size > Number.MAX_SAFE_INTEGER) {
+            throw new Error('The number is too large to convert.')
+        }
+    }
+    while (size >= 1024) {
+        size = size / 1024
+        index++
+    }
+    if (size < 0.01) {
+        return "--"
+    }
+    return size.toFixed(2) + unit[index]
+}
+
+// 单位转目标单位:bit 转 GB、TB、PB、EB、ZB、YB
+export const convertSizeToTargetUnit = (size: number, targetUnit: string): number => {
+    const unit = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    let index = 0,
+        convertedSize: number = 0
+    const targetIndex = unit.indexOf(targetUnit.toUpperCase())
+
+    if (targetIndex !== -1) {
+        while (index <= targetIndex) {
+            size = size / 1024
+            index++
+        }
+        convertedSize = parseFloat(size.toFixed(2))
+    }
+
+    return convertedSize
+}
+
+export function getUserInfo() {
+    const userInfo = localStorage.getItem('user')
+    if (userInfo) {
+        return JSON.parse(userInfo)
+    }
+    return null
+}
