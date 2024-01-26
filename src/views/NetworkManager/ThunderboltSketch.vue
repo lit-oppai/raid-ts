82241a97cf7e82a8f72b18cd9768173c9d0e4e53
zhanghengxin <ezreal.zhang@icewhale.org>
2024-01-26 11:17:34 +0000

    Fix Thunderbolt port name display

--- src/views/NetworkManager/ThunderboltSketch.vue
+++ src/views/NetworkManager/ThunderboltSketch.vue
@@ -16,7 +16,7 @@ defineProps({
 <template>
     <div class="relative">
         <div class="absolute left-[134px] top-[17px] focus:outline-none">
-            <ThunderboltPortStatus v-for="data in ThunderboltData" :name="data.product" :state="data.ip !== ''" :theoretical-speed="data.theoretical_speed"
+            <ThunderboltPortStatus v-for="data in ThunderboltData" :name="data.product || data.name" :state="data.ip !== ''" :theoretical-speed="data.theoretical_speed"
                 :negotiated-speed="data.negotiated_speed" />
         </div>
         <Image :src="ThunderboltSVG" />
