16f6406ce301f8846c3beb3540af8f5aba5ea101
zhanghengxin <ezreal.zhang@icewhale.org>
2024-02-28 08:12:59 +0000

    Add dependencies and update UI components

--- src/network/index.ts
+++ src/network/index.ts
@@ -2,6 +2,7 @@ import { api, useInstance } from '@icewhale/ui-utils';
 import { Configuration, DiskMethodsApi, MergeMethodsApi, MountMethodsApi, StorageMethodsApi, RaidMethodsApi } from "@icewhale/zimaos-localstorage-openapi";
 import { NetworkMethodsApi, ZerotierMethodsApi, DeviceMethodsApi } from "@icewhale/zimaos-openapi"
 import { DriveMethodsApi } from "@icewhale/icewhale-openapi/generate/icewhale-drive";
+import { DefaultApi as InstallApi } from "@icewhale/casaos-installer-openapi";
 
 const instance = useInstance();
 const configuration = new Configuration({
@@ -20,6 +21,8 @@ export const network = new NetworkMethodsApi(configuration, "/v2/zimaos", instan
 export const zerotier = new ZerotierMethodsApi(configuration, "/v2/zimaos", instance);
 export const device = new DeviceMethodsApi(configuration, "/v2/zimaos", instance);
 export const drive = new DriveMethodsApi(configuration, "/v2/icewhale", instance);
+
+export const install = new InstallApi(configuration, "/v2/installer", instance);
 
 export {
     api,
