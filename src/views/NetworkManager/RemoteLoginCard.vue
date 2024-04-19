3f95eb72b88be8970453570d0311d037bd7534b4
zhanghengxin <ezreal.zhang@icewhale.org>
2024-04-19 20:46:05 +0800

    feat ：1 、Refactor NetworkManager.vue to update remote access status in RemoteLoginCard.vue and add overlay panel for reset ID option

--- src/views/NetworkManager/RemoteLoginCard.vue
+++ src/views/NetworkManager/RemoteLoginCard.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
-import copy                                      from "clipboard-copy";
-import { ref, onMounted, defineEmits, watch }    from "vue";
-import Image                                     from "primevue/image";
-import InputSwitch                               from "primevue/inputswitch";
-import { NPopover }                              from "naive-ui";
-import remoteSVG                                 from "@/assets/img/NetworkManager/remote.svg";
-import { useToast }                              from "primevue/usetoast";
-import { zerotierAPI }                           from "@network/index.ts";
-import { ZTInfo }                                from "@icewhale/zimaos-openapi";
+import copy                                   from "clipboard-copy";
+import { ref, onMounted, defineEmits, watch } from "vue";
+import Image                                  from "primevue/image";
+import InputSwitch                            from "primevue/inputswitch";
+import OverlayPanel                           from "primevue/overlaypanel";
+import { NPopover }                           from "naive-ui";
+import remoteSVG                              from "@/assets/img/NetworkManager/remote.svg";
+import { useToast }                           from "primevue/usetoast";
+import { zerotierAPI }                        from "@network/index.ts";
+import { ZTInfo }                             from "@icewhale/zimaos-openapi";
 
 const toast = useToast();
 const accessID = ref<string>("");
 const isAccessed = ref<boolean>(false);
 const isChecking = ref<boolean>(false);
+const op = ref();
 const emit = defineEmits(["update:remoteAccessStatus"]);
 let clickedCount = 0;
 let lastClickTime = 0;
@@ -25,7 +27,7 @@ onMounted(() => {
             accessID.value = data?.id || "";
             isAccessed.value = data?.status === "online";
         }
-    })
+    });
 });
 
 watch(isAccessed, () => {
@@ -96,6 +98,10 @@ function switchAccess() {
             isChecking.value = false;
         });
 }
+
+function handleMenuClick(e: Event) {
+    op.value.toggle(e);
+}
 </script>
 <template>
     <div
@@ -114,14 +120,14 @@ function switchAccess() {
                 ></div>
             </div>
 
-            <div class="flex items-center mt-1 h-[18px]" v-show="accessID">
+            <div class="flex items-center mt-1 h-[18px] space-x-1" v-show="accessID">
                 <div class="text-neutral-400 text-xs font-normal">
-                    ID : &nbsp;
+                    ID :
                 </div>
                 <NPopover trigger="hover" placement="right">
                     <template #trigger>
                         <div
-                            class="flex border border-solid rounded border-gray-300"
+                            class="flex border border-solid rounded border-Gary/500"
                         >
                             <div
                                 class="text-zinc-800 text-xs font-normal flex self-center pl-2 pr-1.5"
@@ -129,7 +135,7 @@ function switchAccess() {
                                 {{ accessID }}
                             </div>
                             <i
-                                class="text-base text-center leading-4 casa-copy-outline bg-gray-300 cursor-pointer"
+                                class="text-base text-center leading-4 casa-copy-outline bg-Gary/500 cursor-pointer"
                                 @click.native="handleClick"
                             >
                             </i>
@@ -140,6 +146,12 @@ function switchAccess() {
                         v-t="`Copy`"
                     ></div>
                 </NPopover>
+                <i class="casa-dots-vertical-outline hover:bg-Gary/300 active:bg-Gary/500 cursor-pointer w-[18px] h-[18px] rounded flex items-center justify-center" @click="handleMenuClick"></i>
+                <OverlayPanel ref="op" id="overlay_tmenu" class="mt-1">
+                    <div class="bg-Gary/Primary flex items-center p-1 rounded-md outline outline-Gary/500 cursor-pointer shadow-2xl select-none">
+                        <span class="text-white text-center text-xs font-normal leading-4 px-1 py-0.5 hover:bg-Gary/Normal rounded">Reset ID</span>
+                    </div>
+                </OverlayPanel>
             </div>
         </div>
 
