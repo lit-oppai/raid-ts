17ae4fa506f42696c051f42017262bd008aa1e08
zhanghengxin <ezreal.zhang@icewhale.org>
2024-04-26 15:21:55 +0800

    Fix: Cache the first data loaded. Fix: Fix error positions while in the main frame.

--- src/views/NetworkManager/RemoteLoginCard.vue
+++ src/views/NetworkManager/RemoteLoginCard.vue
@@ -1,18 +1,35 @@
 <script setup lang="ts">
-import copy                                   from "clipboard-copy";
-import { ref, onMounted, defineEmits, watch } from "vue";
-import Image                                  from "primevue/image";
-import InputSwitch                            from "primevue/inputswitch";
-import OverlayPanel                           from "primevue/overlaypanel";
-import { NPopover }                           from "naive-ui";
-import remoteSVG                              from "@/assets/img/NetworkManager/remote.svg";
-import { useToast }                           from "primevue/usetoast";
-import { zerotierAPI }                        from "@network/index.ts";
-import { ZTInfo }                             from "@icewhale/zimaos-openapi";
+import copy            from "clipboard-copy";
+import {
+    ref,
+    Ref,
+    computed,
+    ComputedRef,
+    onMounted,
+    defineEmits,
+    watch,
+} from "vue";
+import Image           from "primevue/image";
+import InputSwitch     from "primevue/inputswitch";
+import OverlayPanel    from "primevue/overlaypanel";
+import { NPopover }    from "naive-ui";
+import remoteSVG       from "@/assets/img/NetworkManager/remote.svg";
+import { useToast }    from "primevue/usetoast";
+import { zerotierAPI } from "@network/index.ts";
+import { ZTInfo }      from "@icewhale/zimaos-openapi";
+import { useStorage }  from "@vueuse/core";
 
 const toast = useToast();
-const accessID = ref<string>("");
-const isAccessed = ref<boolean>(false);
+const zerotier_info: Ref<ZTInfo> = useStorage(
+    "zerotier_info_api",
+    {},
+    sessionStorage
+);
+
+const accessID: ComputedRef<string> = computed(
+    () => zerotier_info.value?.id || ""
+);
+const isAccessed = ref<boolean>(zerotier_info.value.status === "online");
 const isChecking = ref<boolean>(false);
 const op = ref();
 const emit = defineEmits(["update:remoteAccessStatus"]);
@@ -20,19 +37,34 @@ let clickedCount = 0;
 let lastClickTime = 0;
 
 onMounted(() => {
-    zerotierAPI.getZerotierInfo().then((res: any) => {
-        if (res.status === 200) {
-            const data: ZTInfo = res.data;
-            // id must be exist.
-            accessID.value = data?.id || "";
-            isAccessed.value = data?.status === "online";
-        }
-    });
+    zerotierAPI
+        .getZerotierInfo()
+        .then((res: any) => {
+            if (res.status === 200) {
+                const data: ZTInfo = res.data;
+                // id must be exist.
+                // accessID.value = data?.id || "";
+                // isAccessed.value = data?.status === "online";
+                zerotier_info.value = data;
+            }
+        })
+        .catch((err: any) => {
+            zerotier_info.value = { id: "", status: "offline" };
+            console.log(err, "color:#0f0");
+        });
 });
 
-watch(isAccessed, () => {
-    emit("update:remoteAccessStatus", isAccessed.value);
-});
+watch(
+    zerotier_info.value,
+    (value) => {
+        isAccessed.value = value.status === "online";
+        emit("update:remoteAccessStatus", isAccessed.value);
+    },
+    { deep: true, immediate: true }
+);
+// watch(isAccessed, (value) => {
+//     emit("update:remoteAccessStatus", value);
+// });
 
 function handleClick() {
     const currentTime = Date.now();
@@ -63,15 +95,16 @@ function handleClick() {
 function changeAccessId() {
     zerotierAPI
         .setZerotierNetworkStatus({ status: "reset" })
-        .then((res:any) => {
+        .then((res: any) => {
             if (res.status === 200) {
-                isAccessed.value = false;
-                accessID.value = "";
+                // isAccessed.value = false;
+                // accessID.value = "";
+                zerotier_info.value = res.data;
             }
         })
         .finally(() => {
             isChecking.value = false;
-            op.value.toggle();
+            op.value.hide();
         });
 }
 
@@ -83,8 +116,9 @@ function switchAccess() {
         .setZerotierNetworkStatus({ status })
         .then((res: any) => {
             if (res.status === 200) {
-                isAccessed.value = res.data.status === "online";
-                accessID.value = res.data?.id || "";
+                // isAccessed.value = res.data.status === "online";
+                // accessID.value = res.data?.id || "";
+                zerotier_info.value = res.data;
             }
         })
         .catch((err: any) => {
@@ -121,10 +155,11 @@ function handleMenuClick(e: Event) {
                 ></div>
             </div>
 
-            <div class="flex items-center mt-1 h-[18px] space-x-1" v-show="accessID">
-                <div class="text-neutral-400 text-xs font-normal">
-                    ID :
-                </div>
+            <div
+                class="flex items-center mt-1 h-[18px] space-x-1"
+                v-show="accessID"
+            >
+                <div class="text-neutral-400 text-xs font-normal">ID :</div>
                 <NPopover trigger="hover" placement="right">
                     <template #trigger>
                         <div
@@ -147,12 +182,22 @@ function handleMenuClick(e: Event) {
                         v-t="`Copy`"
                     ></div>
                 </NPopover>
-                <i class="casa-dots-vertical-outline hover:bg-Gary/300 active:bg-Gary/500 cursor-pointer w-[18px] h-[18px] rounded flex items-center justify-center" @click="handleMenuClick"></i>
-                <OverlayPanel ref="op" id="overlay_tmenu" class="mt-1">
-                    <div class="bg-Gary/Primary flex items-center p-1 rounded-md outline outline-Gary/500 cursor-pointer shadow-2xl select-none" @click="changeAccessId">
-                        <span class="text-white text-center text-xs font-normal leading-4 px-1 py-0.5 hover:bg-Gary/Normal rounded">Reset ID</span>
-                    </div>
-                </OverlayPanel>
+                <i
+                    class="casa-dots-vertical-outline hover:bg-Gary/300 active:bg-Gary/500 cursor-pointer w-[18px] h-[18px] rounded flex items-center justify-center relative"
+                    @click="handleMenuClick"
+                >
+                    <OverlayPanel ref="op" class="mt-1 !top-[22px] !left-0" append-to="self">
+                        <div
+                            class="bg-Gary/Primary flex items-center p-1 rounded-md outline outline-Gary/500 cursor-pointer shadow-2xl select-none"
+                            @click="changeAccessId"
+                        >
+                            <span
+                                class="text-white text-center text-xs font-normal leading-4 px-1 py-0.5 hover:bg-Gary/Normal rounded text-nowrap"
+                                >Reset ID</span
+                            >
+                        </div>
+                    </OverlayPanel>
+                </i>
             </div>
         </div>
 
